b98ee837195224c67620ff9d8f5bdcd6c05f949c
[ANANDGANDHIK] <[anandgandhik@gmail.com]>
2024-05-27 04:50:24 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    if (window.screen.width < 1366 || window.screen.height < 768) {
+        // Display a message to the user or redirect them
+        document.body.innerHTML = "Your screen resolution is less than 1366x768. Please increase your screen resolution to view this website.";
+    } else {
+        // Load your 3D website
+    }
+}
